332a27882fbf8fb292fb9ea4c3806ea2a26d3dec
saahilanande <saahilanande21@gmail.com>
2023-05-10 16:31:49 -0500

    Server.js File Added with Basic Setup, .env File Added for setting test variables, Dev script added in package.json for dev run

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+// using for allowing cross site request from anywhere
+app.use(cors());
+
+// Getting the mongodb connection string from environment varaible
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true });
+
+//Creating a connection object of mongoose
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("mongodb connected succesfully");
+});
+
+//Base route
+app.get("/", (req, res) => {
+  res.send("Coursepedia on server");
+});
+
+//Allowing the server to listen to a specific port, Default 3000 if not provided
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on: ${port}`);
+});
