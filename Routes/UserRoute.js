9acf948495af31ef92e0a01237818ee66dd0514e
Abdul Gafoor <gafoor.kamran@gmail.com>
2023-05-13 22:07:32 -0500

    Update validation v1

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { Router } = require("express");
 const user = require("../Modal/UserModal");
+const Joi = require("joi");
 
 //Endpoint to get a user by Id
 router.route("/:id").get((req, res) => {
@@ -56,17 +57,34 @@ router.route("/adduser").post((req, res) => {
 //Endpoint to update a new user
 
 router.route("/update/:id").put((req, res) => {
-  user
-    .findByIdAndUpdate(req.params.id, req.body, { new: true })
-    .then((user) => {
-      if (!user) {
-        return res.status(404).json("User updated with ID:" + user);
-      }
-      res.send(user);
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
+  //Joi schema created for incoming post object
+  const schema = Joi.object({
+    name: Joi.string().min(2).required(),
+    email: Joi.string().min(2).required(),
+    password: Joi.string().alphanum().required(),
+    phone: Joi.number().integer(),
+    age: Joi.number().integer().max(100),
+  });
+
+  //validate the object recevied during post request
+
+  const validation = schema.validate(req.body);
+  console.log(validation.error);
+  if (validation.error) {
+    user
+      .findByIdAndUpdate(req.params.id, req.body, { new: true })
+      .then((user) => {
+        if (!user) {
+          return res.status(404).json("User updated with ID:" + user);
+        }
+        res.send(user);
+      })
+      .catch((error) => {
+        res.status(500).send(error);
+      });
+  } else {
+    res.send("Invalid data");
+  }
 });
 
 module.exports = router;
